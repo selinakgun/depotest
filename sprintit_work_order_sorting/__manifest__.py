591e96fe6cff8c5526b9ec9ebbc3d449f6288777
selinakgun <44244320+selinakgun@users.noreply.github.com>
2021-01-20 16:29:00 +0300

    [ADD] app sprintit_work_order_sorting

---
+++ sprintit_work_order_sorting/__manifest__.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    ODOO Open Source Management Solution
+#
+#    ODOO Addon module by Sprintit Ltd
+#    Copyright (C) 2020 Sprintit Ltd (<http://sprintit.fi>).
+#
+##############################################################################
+
+
+{
+    'name': 'Sprintit Work Order Sorting',
+    'version': '12.0.1.0',
+    'license': 'LGPL-3',
+    'category': 'Manufacturing',
+    'description': 'MRP production Work Order Sorting based on Deadline Start Date',
+    'author': 'Sprintit ltd',
+    'maintainer': 'SprintIT',
+    'website': 'https://sprintit.fi/in-english',
+    'depends': [ 
+        'mrp',
+    ],
+    'data': [
+        'view/assets.xml',
+        'view/mrp_view.xml',
+        
+    ],
+    'demo': [
+    ],
+    'test': [
+    ],
+    'images': ['static/description/cover.jpg',],
+    "external_dependencies": { # python pip packages
+    #     'python': ['suds', 'dateutil'],
+    },
+    'installable': True,
+    'auto_install': False,
+    'price': 0.0,
+    'currency': 'EUR',    
+ }
+
